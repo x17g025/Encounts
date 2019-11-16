1c4bc2c1c136bd86aef79ad07f4c9b37bb47ac17
Yuta Nakamura <x17g025@chiba-fjb.ac.jp>
2019-11-16 16:08:34 +0900

    11/16

--- app/src/main/java/com/example/encount/UserProfile.kt
+++ app/src/main/java/com/example/encount/UserProfile.kt
@@ -5,14 +5,14 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.gson.Gson
-import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import org.w3c.dom.Text
 import java.io.IOException
 
 class UserProfile : AppCompatActivity() {
@@ -23,19 +23,53 @@ class UserProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
-        UserDataPost().execute()
+        val actPost   = findViewById<View>(R.id.PostLine)
+        val actFriend = findViewById<View>(R.id.FriendLine)
+        val actStar   = findViewById<View>(R.id.LikeLine)
 
-        prfCng.setOnClickListener {
+        val userPostBtn       = findViewById<LinearLayout>(R.id.UserPost)
+        val userFriendBtn     = findViewById<LinearLayout>(R.id.UserFriend)
+        val userLikeBtn       = findViewById<LinearLayout>(R.id.UserLike)
+        val userSettingsBtn   = findViewById<ImageView>(R.id.UserSettings)
 
-            //3.Intentクラスのオブジェクトを生成。
-            val intent = Intent(this, UserProfileChange::class.java)
-            //生成したオブジェクトを引数に画面を起動！
-            startActivity(intent)
+        actFriend.visibility = View.GONE
+        actStar.visibility   = View.GONE
+
+        UserDataGet().execute()
+        UserPostData().execute()
+
+        userPostBtn.setOnClickListener {
+            actPost.visibility   = View.VISIBLE
+            actFriend.visibility = View.GONE
+            actStar.visibility   = View.GONE
+
+            UserPostData().execute()
+        }
+
+        userFriendBtn.setOnClickListener {
+            actPost.visibility   = View.GONE
+            actFriend.visibility = View.VISIBLE
+            actStar.visibility   = View.GONE
+
+            UserFriendData().execute()
+        }
+
+        userLikeBtn.setOnClickListener {
+            actPost.visibility   = View.GONE
+            actFriend.visibility = View.GONE
+            actStar.visibility   = View.VISIBLE
+
+            UserLikeData().execute()
+        }
+
+        userSettingsBtn.setOnClickListener {
+
+            startActivity(Intent(this, UserProfileChange::class.java))
         }
 
     }
 
-    private inner class UserDataPost() : AsyncTask<String, String, String>() {
+    private inner class UserDataGet() : AsyncTask<String, String, String>() {
 
         override fun doInBackground(vararg params: String): String {
 
@@ -78,11 +112,165 @@ class UserProfile : AppCompatActivity() {
         }
 
         override fun onPostExecute(result: String) {
-            val etName = findViewById<TextView>(R.id.userName)
-            val etAtname = findViewById<TextView>(R.id.userAtName)
+
+            val txName = findViewById<TextView>(R.id.UserName)
+            val txBio = findViewById<TextView>(R.id.UserBio)
             val userData = Gson().fromJson(result, UserDataClassList::class.java)
-            etName.text = userData.userName
-            etAtname.text = userData.userAtname
+            txName.text = userData.userName
+            txBio.text = userData.userBio
         }
     }
+
+    private inner class UserPostData() : AsyncTask<String, String, String>() {
+
+        override fun doInBackground(vararg params: String): String {
+
+            var id = ""
+            val db = _helper.writableDatabase
+            val sql = "select * from userInfo"
+            val cursor = db.rawQuery(sql, null)
+
+            while(cursor.moveToNext()){
+                val idxId = cursor.getColumnIndex("user_id")
+
+                id = cursor.getString(idxId)
+                Log.d("DebugUser",id)
+            }
+
+            val client = OkHttpClient()
+
+            //アクセスするURL
+            val url = "https://kinako.cf/UserDataGet.php"
+
+            //Formを作成
+            val formBuilder = FormBody.Builder()
+
+            //formに要素を追加
+            formBuilder.add("id",id)
+            //リクエストの内容にformを追加
+            val form = formBuilder.build()
+
+            //リクエストを生成
+            val request = Request.Builder().url(url).post(form).build()
+
+            try {
+                val response = client.newCall(request).execute()
+                return response.body()!!.string()
+            }
+            catch (e: IOException) {
+                e.printStackTrace()
+                return "Error"
+            }
+        }
+
+        override fun onPostExecute(result: String) {
+
+            val etName = findViewById<TextView>(R.id.UserName)
+            val userData = Gson().fromJson(result, UserDataClassList::class.java)
+            etName.text = userData.userName
+        }
+    }
+
+    private inner class UserFriendData() : AsyncTask<String, String, String>() {
+
+        override fun doInBackground(vararg params: String): String {
+
+            var id = ""
+            val db = _helper.writableDatabase
+            val sql = "select * from userInfo"
+            val cursor = db.rawQuery(sql, null)
+
+            while(cursor.moveToNext()){
+                val idxId = cursor.getColumnIndex("user_id")
+
+                id = cursor.getString(idxId)
+                Log.d("DebugUser",id)
+            }
+
+            val client = OkHttpClient()
+
+            //アクセスするURL
+            val url = "https://kinako.cf/UserDataGet.php"
+
+            //Formを作成
+            val formBuilder = FormBody.Builder()
+
+            //formに要素を追加
+            formBuilder.add("id",id)
+            //リクエストの内容にformを追加
+            val form = formBuilder.build()
+
+            //リクエストを生成
+            val request = Request.Builder().url(url).post(form).build()
+
+            try {
+                val response = client.newCall(request).execute()
+                return response.body()!!.string()
+            }
+            catch (e: IOException) {
+                e.printStackTrace()
+                return "Error"
+            }
+        }
+
+        override fun onPostExecute(result: String) {
+
+            val etName = findViewById<TextView>(R.id.UserName)
+            val userData = Gson().fromJson(result, UserDataClassList::class.java)
+            etName.text = userData.userName
+        }
+    }
+
+    private inner class UserLikeData() : AsyncTask<String, String, String>() {
+
+        override fun doInBackground(vararg params: String): String {
+
+            var id = ""
+            val db = _helper.writableDatabase
+            val sql = "select * from userInfo"
+            val cursor = db.rawQuery(sql, null)
+
+            while(cursor.moveToNext()){
+                val idxId = cursor.getColumnIndex("user_id")
+
+                id = cursor.getString(idxId)
+                Log.d("DebugUser",id)
+            }
+
+            val client = OkHttpClient()
+
+            //アクセスするURL
+            val url = "https://kinako.cf/UserDataGet.php"
+
+            //Formを作成
+            val formBuilder = FormBody.Builder()
+
+            //formに要素を追加
+            formBuilder.add("id",id)
+            //リクエストの内容にformを追加
+            val form = formBuilder.build()
+
+            //リクエストを生成
+            val request = Request.Builder().url(url).post(form).build()
+
+            try {
+                val response = client.newCall(request).execute()
+                return response.body()!!.string()
+            }
+            catch (e: IOException) {
+                e.printStackTrace()
+                return "Error"
+            }
+        }
+
+        override fun onPostExecute(result: String) {
+
+            val etName = findViewById<TextView>(R.id.UserName)
+            val userData = Gson().fromJson(result, UserDataClassList::class.java)
+            etName.text = userData.userName
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/encount/UserProfileChange.kt
+++ app/src/main/java/com/example/encount/UserProfileChange.kt
@@ -1,5 +1,6 @@
 package com.example.encount
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,5 +10,7 @@ class UserProfileChange : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile_change)
+
+        startActivity(Intent(this, UserLogin::class.java))
     }
 }
