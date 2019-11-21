c26e5fd2604f41c1aa6014152c80bcdbd870f6e3
Yuta <x17g025@chiba-fjb.ac.jp>
2019-11-21 11:00:37 +0900

    11/21

--- app/src/main/java/com/example/encount/PostAdapter.kt
+++ app/src/main/java/com/example/encount/PostAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.encount
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_user_settings.*
 import kotlinx.android.synthetic.main.post_list.view.*
 
 class PostAdapter(val context: Context, val posts: List<post>): BaseAdapter() {
@@ -30,6 +32,10 @@ class PostAdapter(val context: Context, val posts: List<post>): BaseAdapter() {
         view.PostUserName.text = posts[position].name
         view.PostUserText.text = posts[position].text
         Glide.with(context).load(posts[position].image).into(view.PostImage)
+        view.PostLike.setOnClickListener {
+
+            PostLikeImage.
+        }
         return view
     }
 }
