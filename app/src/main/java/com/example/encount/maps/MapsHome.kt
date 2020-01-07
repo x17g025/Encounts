13548752dd5e762eb80268b9ab25e020cb93fc90
mst <x17g006@chiba-fjb.ac.jp>
2020-01-08 00:09:58 +0900

    ＜変更点＞ ・スポット詳細画面のGridViewが正しく表示できるようになった  （設定されている座標から半径25m以内の写真を表示する） ・マップ画面に処理を追加

--- app/src/main/java/com/example/encount/maps/MapsHome.kt
+++ app/src/main/java/com/example/encount/maps/MapsHome.kt
@@ -14,6 +14,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.encount.MapsDataClassList
 import com.example.encount.MapsList
+import com.example.encount.PostList2
 import com.example.encount.R
 import com.example.encount.post.UserPost
 import com.google.android.gms.location.*
@@ -24,10 +25,12 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_maps_home.*
+import kotlinx.android.synthetic.main.spotmain.*
 import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
+import java.lang.Exception
 
 class MapsHome : Fragment(), OnMapReadyCallback {
 
@@ -56,6 +59,7 @@ class MapsHome : Fragment(), OnMapReadyCallback {
             val intent = Intent(context, SpotMainActivity::class.java)
             startActivity(intent)
         }
+
         // Android 6, API 23以上でパーミッションの確認
         if (Build.VERSION.SDK_INT >= 23) {
             val permissions = arrayOf(
@@ -114,6 +118,7 @@ class MapsHome : Fragment(), OnMapReadyCallback {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
+    //下の処理を再起的に利用してマップ上に写真表示を行う
     //default location
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -162,6 +167,78 @@ class MapsHome : Fragment(), OnMapReadyCallback {
         fun setPostList(mapsHome: MapsHome, mutableList: MutableList<MapsList>) {
             mapsHome.postList = mapsHome.postList
             Log.d("debug", "pass" + mapsHome.postList[1].imgpath)
+        }
+    }
+
+
+    /**
+     * ここから下は、現在地をサーバに送信し、現在地より50m以内で投稿されている写真の情報をサーバから取得する処理にしたい。
+     *
+     *現状ではスポット詳細画面から処理をコピペしただけ
+     */
+    private inner class SpotPhotoGet() : AsyncTask<String, String, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+            val client = OkHttpClient()
+
+            //アクセスするURL
+            val url = "https://encount.cf/encount/SpotInfoSend.php"
+
+            //Formを作成
+            val formBuilder = FormBody.Builder()
+
+            println("経度" + latitude.toString())
+            println("緯度" + longitude.toString())
+
+            //Formに要素を追加
+            formBuilder.add("latitude", latitude.toString())
+            formBuilder.add("longitude", longitude.toString())
+
+            //リクエスト内容にformを追加
+            val form = formBuilder.build()
+
+            //リクエストを生成
+            val request = Request.Builder().url(url).post(form).build()
+
+            try {
+                val response = client.newCall(request).execute()
+                println(url)
+                return response.body()!!.string()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                return "Error"
+            }
+        }
+
+
+        override fun onPostExecute(result: String) {
+            try {
+                var postList = mutableListOf<PostList2>()
+                val listType = object : TypeToken<List<PostList2>>() {}.type
+                val postData = Gson().fromJson<List<PostList2>>(result, listType)
+                var postCount = 0
+
+                for (i in postData) {
+
+                    postCount++
+
+                    postList.add(
+                        PostList2(
+                            i.imageId,
+                            i.userId,
+                            i.imagePath,
+                            i.imageLat,
+                            i.imageLng
+                        )
+                    )
+                }
+
+                SpotPopularCount.text = Integer.toString(postCount)
+                SpotNewCount.text = Integer.toString(postCount)
+                //gridview.adapter = GridAdapter(this@SpotMainActivity, postList)
+            } catch (e: Exception) {
+
+            }
         }
     }
 }
