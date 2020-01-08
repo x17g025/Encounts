17e2fc72979cb4f60995ee419b7936695b52c4b2
x17g025 <x17g025@chiba-fjb.ac.jp>
2020-01-08 12:35:08 +0900

    01/08

---
+++ app/src/main/java/com/example/encount/user/ProfileChange.kt
@@ -0,0 +1,13 @@
+package com.example.encount.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.encount.R
+
+class ProfileChange : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_change)
+    }
+}
